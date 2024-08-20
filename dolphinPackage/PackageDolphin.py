0e9c6c0a6ba01c605913a6e01cf3385d4c590e7c
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-08-20 00:38:30 -0400

    Update PackageDolphin.py

--- dolphinPackage/PackageDolphin.py
+++ dolphinPackage/PackageDolphin.py
@@ -3,9 +3,23 @@
 import os
 import subprocess
 
+#packages the distribution build
 uncompressedPackageFP = "KARphin"
 tarPackageFP = "KARphin.tar"
 brotliPackageFP = "KARphin.br"
+
+#pack into TAR
+command = ['tar', '-cvf', tarPackageFP, uncompressedPackageFP]
+subprocess.run(command)
+
+#pack TAR into Brotli
+command = ['brotli', '-q', '5', '-f', tarPackageFP, '-o', brotliPackageFP]
+subprocess.run(command)
+
+#packages the dev build
+uncompressedPackageFP = "KARphinDev"
+tarPackageFP = "KARphinDev.tar"
+brotliPackageFP = "KARphinDev.br"
 
 #pack into TAR
 command = ['tar', '-cvf', tarPackageFP, uncompressedPackageFP]

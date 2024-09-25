da0699a9f0b1d274e1f7e4ec1f9efb0edba5e1d9
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-09-25 07:17:28 -0400

    removed excess vars from Account

--- Source/Core/DolphinQt/NetPlay/KARAccount.h
+++ Source/Core/DolphinQt/NetPlay/KARAccount.h
@@ -28,32 +28,13 @@ private:
 
   void SaveSettings();
 
-  // Main Widget
+
   QDialogButtonBox* m_button_box;
-  QComboBox* m_connection_type;
+
   QLineEdit* m_nickname_edit;
   QGridLayout* m_main_layout;
-  QTabWidget* m_tab_widget;
-  QPushButton* m_reset_traversal_button;
 
-  // Connection Widget
-  QLabel* m_ip_label;
-  QLineEdit* m_ip_edit;
-  QLabel* m_connect_port_label;
-  QSpinBox* m_connect_port_box;
   QPushButton* m_connect_button;
 
-  // Host Widget
-  QLabel* m_host_port_label;
-  QSpinBox* m_host_port_box;
-  QListWidget* m_host_games;
-  QPushButton* m_host_button;
-  QCheckBox* m_host_force_port_check;
-  QSpinBox* m_host_force_port_box;
-  QCheckBox* m_host_chunked_upload_limit_check;
-  QSpinBox* m_host_chunked_upload_limit_box;
-  QCheckBox* m_host_server_browser;
-  QLineEdit* m_host_server_name;
-  QLineEdit* m_host_server_password;
   QComboBox* m_host_server_region;
 };

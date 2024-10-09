8701bb1d56cca8231f93c5024314dd02e44b025f
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2024-10-09 00:30:11 -0400

    added game IDs for supported ROMs

---
+++ Source/Core/KAR/GameIDs.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+//defines various IDs of the supported ROMs
+
+#include <string>
+
+namespace KAR::Data::GameID
+{
+	static const std::string GAME_ID_HACK_PACK = "KHPE01";  // defines the Hack Pack game ID
+	
+	static const std::string GAME_ID_BACKSIDE = "KBSE01";  // defines the Backside game ID
+	
+	static const std::string GAME_ID_IGNITION = "SLAV01";  // defines the Ignition game ID
+
+	static const std::string GAME_ID_NA = "GKYE01";   // defines the North America game ID
+
+	static const std::string GAME_ID_JP = "GKYJ01";   // defines the Japanease game ID
+
+	static const std::string GAME_ID_PAL = "GKYP01";  // defines the Europe game ID
+}
